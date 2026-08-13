ff4226cc374bd396b34dd4b9d55308b779a4989f
samer <judosamer555@gmail.com>
2026-08-13 07:07:40 +0300

    first commit

---
+++ src/main/java/com/jobportal/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.jobportal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureApiVersioning(ApiVersionConfigurer configurer) {
+        configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.eazyapp+json"), "v")
+                .addSupportedVersions("1.0","2.0","3.0").setDefaultVersion("1.0");
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api",_->true);
+    }
+}
