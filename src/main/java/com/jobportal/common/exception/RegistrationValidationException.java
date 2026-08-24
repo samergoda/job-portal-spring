dbc4fc5c43d98d2c4f720d4655374e01ac43a171
samer <s.gouda@boutiqaat.com>
2026-08-24 16:27:58 +0300

    add aspects

---
+++ src/main/java/com/jobportal/common/exception/RegistrationValidationException.java
@@ -0,0 +1,18 @@
+package com.jobportal.common.exception;
+
+
+import java.util.Map;
+
+public class RegistrationValidationException extends RuntimeException {
+
+    private final Map<String, String> errors;
+
+    public RegistrationValidationException(Map<String, String> errors) {
+        super("Registration validation failed");
+        this.errors = errors;
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+}
